dd4976a897c8f3aa85915fc8e218ecb8f33538ab
Chandra Prakash Singh <cprakashsingh2306@gmail.com>
2024-05-29 17:27:31 +0530

    hello

--- studybud/settings.py
+++ studybud/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 import os
-STATIC_URL = 'static/'
+STATIC_URL = 'theme/static/'
 MEDIA_URL = '/image/'
 STATICFILES_DIRS=[
-    BASE_DIR / 'static'
+    BASE_DIR / 'theme/static'
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
